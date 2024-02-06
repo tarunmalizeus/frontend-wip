377019613220e90f0990c2e0bd4908bd84553b9e
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-06 19:30:41 +0530

    do banner 2

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,10 +1,15 @@
-
+import { Link } from 'react-router-dom'
 
 function Landingpage() {
     return (
       <>
         <h1>Landingpage</h1>
-        
+        <button>
+          <Link to="/signup">Signup</Link>
+        </button>
+        <button>
+          <Link to="/login">Login</Link>
+        </button>        
       </>
     )
   }
