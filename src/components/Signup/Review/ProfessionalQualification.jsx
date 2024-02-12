0d58f6043b7714602c22763600b22c5c4b6d44da
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-12 15:41:36 +0530

    q

--- src/components/Signup/Review/ProfessionalQualification.jsx
+++ src/components/Signup/Review/ProfessionalQualification.jsx
@@ -24,7 +24,7 @@ function ProfessionalQualification() {
         Years Of Experience*
         </div>
         <input
-        className='my-3  w-1/4'
+        className='w-1/4'
           type="number"
           name="yearsOfExperience"
           value={signupData.yearsOfExperience}
@@ -36,7 +36,7 @@ function ProfessionalQualification() {
         Current CTC (in Rupees)*
         </div>
         <input
-        className='my-3  w-1/4'
+        className='w-1/4'
           type="number"
           name="currentCTC"
           value={signupData.currentCTC}
@@ -49,7 +49,7 @@ function ProfessionalQualification() {
         </div>
 
         <input
-        className='my-3  w-1/4'
+        className='w-1/4'
           type="number"
           name="expectedCTC"
           value={signupData.expectedCTC}
@@ -64,12 +64,98 @@ function ProfessionalQualification() {
         ))}
       </div>
 
+
+
+      <div>
+        <div>
+        If Others, Please Mention
+        </div>
+        <div>
+        {signupData.otherExperiencedTech}
+        </div>
+      </div>
+
+
       <div>
          <div>Select the Technologies You're Familiar In*</div>
-        {signupData.experiencedTech.map((tech, index) => (
+        {signupData.familiarTech.map((tech, index) => (
           <div key={index}>{tech}</div>
         ))}
       </div>
+
+      <div>
+        <div>
+        If Others, Please Mention
+        </div>
+        <div>
+        {signupData.otherFamiliarTech}
+        </div>
+      </div>        
+
+
+      <fieldset className='my-3 space-y-2'>
+        <legend>Are You Currently On Notice Period?</legend>
+        <div>{signupData.onNoticePeriod === 'Yes'?'Yes':null}</div>
+        <div>{signupData.onNoticePeriod === ''?'':null}</div>
+
+          <div className='flex flex-row justify-between'>
+
+            <label>
+              <div>
+
+              If Yes, When Will Your Notice Period End?*
+              </div>
+              <div>
+                {signupData.noticePeriodEnd}
+              </div>
+            </label>
+
+
+            <label className='w-1/2'>
+              <div>
+              How Long is Your Notice Period? (Mention in Months)
+              </div>
+              <div>
+              {signupData.noticePeriodLength}
+              </div>
+            </label>
+          </div>
+      </fieldset>
+      
+      <fieldset>
+        <legend>Have You Appeared for Any Tests in the Past 6 Months?</legend>
+        <label>
+          <div>
+            {signupData.appearedForTests=='Yes'?'Yes':null}
+          </div>
+          <div>
+            {signupData.appearedForTests=='No'?'No':null}
+          </div>
+        </label>
+
+          <label >
+            <div>
+            If Yes, Please Mention The Test Names
+            </div>
+            {console.log(signupData.testNames)}
+            <div>
+              {signupData.testNames===''?'-':signupData.testNames}
+            </div>
+          </label>
+  
+
+
+      </fieldset>    
+
+
+
+
+      {/* <p>{signupData.onNoticePeriod}</p>
+      <p>{signupData.noticePeriodEnd}</p>
+      <p>{signupData.noticePeriodLength}</p>
+      <p>{signupData.appearedForTests}</p>
+      <p>{signupData.testNames}</p> */}
+
 
         </div>
       </div>
