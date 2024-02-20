0c57976e98aac20a9bd650a61dabefe48419a51f
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-20 19:04:15 +0530

    dunzo job view

--- src/pages/Job/JobView.jsx
+++ src/pages/Job/JobView.jsx
@@ -1,65 +1,39 @@
-// import React from 'react';
+import React from 'react';
 import Jobcard from '../../components/job/Jobcard';
-import {useState } from "react";
-import Loading from '../Loading'
-import { useQuery,gql } from "@apollo/client";
+import Loading from '../Loading';
+import { useQuery, gql } from "@apollo/client";
 
-      const QUERY_ALL_JOBS = gql`
-      query Query {
-        jobs {
-          name
-          internship
-          job_id
-          location_id
-          to_time
-          from_time
-        }
-      }
-      `;
+const QUERY_ALL_JOBS = gql`
+  query Query {
+    jobs {
+      name
+      internship
+      job_id
+      location_id
+      to_time
+      from_time
+    }
+  }
+`;
 
 function Jobview() {
-  // const [enrollments, setEnrollments] = useState([]);
-  const {data} = useQuery(QUERY_ALL_JOBS);
-  if(data){
-    console.log(data.jobs)
-  }
+  const { loading, error, data } = useQuery(QUERY_ALL_JOBS);
 
-    // const [loading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     const fetchEnrollments = async () => {
-    //       try {
-    //         // const response = await fetch(`http://localhost:3000/Api/job`);
-    //         // console.log(process.env.REACT_APP_BACKEND)
-    //         const response = await fetch(`${import.meta.env.VITE_BACKEND}/Api/job`);
-    //         if (response.ok) {
-    //           const data = await response.json();
-    //           setEnrollments(data);
-    //           // console.log(data)
-    //           // console.log(enrollments)
-    //         } else {
-    //           console.error('Failed to fetch enrollments');
-    //         }
-    //       } catch (error) {
-    //         console.error('Error during fetch:', error);
-    //       } finally {
-    //         setLoading(false);
-    //       }
-    //     };
-    
-    //     fetchEnrollments();
-    //   }, []);
+  if (loading) return <Loading />;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data.jobs);
 
-      // console.log(enrollments)
-      // console.log(data.jobs)
-        return (
-          <div></div>
-            // !data.jobs ? <Loading /> :(
-            // <div className="flex flex-col">
-            //     {data.jobs.map((dataObj) => (
-            //         <Jobcard key={dataObj.job_id} dataObj={job} />
-            //     ))}
-            // </div>)
-        );
-    }
+  return (
+    <div className="flex flex-col">
+      {data.jobs.length === 0 ? (
+        <p>No jobs available</p>
+      ) : (
+        data.jobs.map((job) => (
+          <Jobcard job={job} />
+        ))
+      )}
+    </div>
+  );
+}
+
 export default Jobview;
-
