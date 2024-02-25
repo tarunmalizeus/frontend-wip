7c131fa0cd05fc791084a233c1dc33c157194a44
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-25 13:38:15 +0530

    working smoothly, experienced memory leak before it

--- src/components/Signup/Banner2.jsx
+++ src/components/Signup/Banner2.jsx
@@ -1,15 +1,22 @@
 import { useSignupNavigation } from "../../utils/SignupNavigationContext.jsx";
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 function Banner2() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { isMainSignup1,isMainSignup2 ,isReview } = useSignupNavigation();
+
+  function setPage(page){
+    navigate(page)
+  }
+
+
     return (
       <>
         <div className="bg-white m-2 mx-4 p-4 flex justify-between px-12"> 
 
         <div
+        onClick={() => setPage('mainsignup2')}
           className="flex gap-2 justify-around items-center cursor-pointer"
         >
        <div
@@ -23,37 +30,7 @@ function Banner2() {
 
 
 
-        {/* <div
-          className="flex gap-2 justify-around items-center cursor-pointer"
-          onClick={() => setTab(2)}
-        >
-          <div
-            className={cn(
-              "flex aspect-square items-center text-center align-middle px-5 py-2.5 bg-[#3FD28B] rounded-full text-xl text-slate-900",
-              { "bg-[#757575] text-white": tab !== 2 }
-            )}
-          >
-            2
-          </div>
-          <div className="text-lg font-bold"> Qualifications</div>
-        </div> */}
 
-
-
-        {/* <div
-          className="flex gap-2 justify-around items-center cursor-pointer"
-          onClick={() => setTab(3)}
-        >
-          <div
-            className={cn(
-              "flex aspect-square items-center text-center align-middle px-5 py-2.5 bg-[#3FD28B] rounded-full text-xl text-slate-900",
-              { "bg-[#757575] text-white": tab !== 3 }
-            )}
-          >
-            3
-          </div>
-          <div className="text-lg font-bold"> Review and Proceed</div>
-        </div> */}
 
         </div>
 
