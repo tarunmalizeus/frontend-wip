5fc3e3ec58a729e2dcfb23a17a688a3ef1696081
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-25 13:48:41 +0530

    banner 2 almost completed

--- src/components/Signup/Banner2.jsx
+++ src/components/Signup/Banner2.jsx
@@ -16,7 +16,7 @@ function Banner2() {
         <div className="bg-white m-2 mx-4 p-4 flex justify-between px-12"> 
 
         <div
-        onClick={() => setPage('mainsignup2')}
+        onClick={() => setPage('')}
           className="flex gap-2 justify-around items-center cursor-pointer"
         >
        <div
@@ -28,6 +28,33 @@ function Banner2() {
           <div className="text-lg font-bold"> Personal Information</div>
         </div> 
 
+
+        <div
+        onClick={() => setPage('mainsignup2')}
+          className="flex gap-2 justify-around items-center cursor-pointer"
+        >
+       <div
+            className= {`flex aspect-square items-center text-center align-middle px-5 py-2.5 ${isMainSignup2? `bg-[#3FD28B]`: `bg-[#757575]` } rounded-full text-xl text-slate-900`}
+          >
+            2
+          </div>
+
+          <div className="text-lg font-bold"> Qualifications</div>
+        </div> 
+
+
+        <div
+        onClick={() => setPage('review')}
+          className="flex gap-2 justify-around items-center cursor-pointer"
+        >
+       <div
+            className= {`flex aspect-square items-center text-center align-middle px-5 py-2.5 ${isReview? `bg-[#3FD28B]`: `bg-[#757575]` } rounded-full text-xl text-slate-900`}
+          >
+            3
+          </div>
+
+          <div className="text-lg font-bold"> Review and Proceed</div>
+        </div> 
 
 
 
