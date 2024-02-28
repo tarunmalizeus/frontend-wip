a27ced2ca1b1918dc2cedd4984f8f2ece67b7f67
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-28 16:47:10 +0530

    moving to apply

--- src/components/Job/Role.jsx
+++ src/components/Job/Role.jsx
@@ -12,8 +12,6 @@ query Query($job_id: Int!, $role_id: Int!) {
   }
 `;
 
-
-
 function Role({ role }) {
     const { role_name, job_id, role_id } = role;
     const { loading, error, data } = useQuery(QUERY_ROLE, {
@@ -23,10 +21,32 @@ function Role({ role }) {
     if (error) return <p>Error: {error.message}</p>;
     return (
         <>
-            <p>{data.roleUnique.package_rupees}</p>
-            <p>{data.roleUnique.description}</p>
-            <p>{data.roleUnique.requirements}</p>
-            <p>{role_name}</p>
+            <div className='space-y-4'>
+        <div className='bg-white shadow-md p-4'>
+          <div className='border-b-2'>
+            <h2 className="text-gray-500 mb-2">Gross Compensation Package:</h2>
+                  <ul className="list-disc list-inside mb-6 space-y-2 list-dash text-xs">
+            <li>Rs {data.roleUnique.package_rupees} lpa</li>
+                  </ul>
+          </div>
+      
+      <div className='border-b-2 pt-4'>
+        <h3 className="text-gray-500 mb-4">Role Description:</h3>
+        <ul className="list-disc list-inside mb-6 space-y-2 list-dash text-xs">
+            <li>{data.roleUnique.description}</li>
+        </ul>
+      </div>
+      
+      <div className='border-b-2 pt-4'>
+        <h3 className="text-gray-500 mb-4">Requirements:</h3>
+        <ul className="list-disc list-inside mb-6 space-y-2 list-dash text-xs">
+            <li>{data.roleUnique.requirements}</li>
+        </ul>
+      </div>
+      
+        </div>
+        </div>
+
         </>
     );
 }
