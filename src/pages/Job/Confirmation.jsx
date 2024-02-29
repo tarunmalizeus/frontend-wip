890c4e289d9732ab1b4d1ae195369fa261964bfb
TarunMali Zeus <tarun.mali@zeuslearning.com>
2024-02-29 12:03:13 +0530

    moving to setting up preferences

--- src/pages/Job/Confirmation.jsx
+++ src/pages/Job/Confirmation.jsx
@@ -2,15 +2,14 @@ import { useJobApplyData } from '../../utils/JobApplyContext';
 
 
 function Confirmation() {
-  const {useJobApplyData } = useJobApplyData();
+  const {jobApplyData} = useJobApplyData();
   return (
     <div className='p-4 m-16 bg-white'>
-      {/* <h1>Confirmation</h1>
       <div>
-        <p>Time Slot: {formData.timeSlot}</p>
+        <p>Time Slot: {jobApplyData.timeSlot}</p>
         <p>Preferences:</p>
-        <p>Resume: {formData.resumeFile ? formData.resumeFile.name : 'None'}</p>
-      </div> */}
+        <p>Resume: {jobApplyData.resumeFile ? jobApplyData.resumeFile.name : 'None'}</p>
+      </div>
     </div>
     )
   }
